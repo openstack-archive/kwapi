810fc5b351f4b9c4def0b6b9a4af08c56eaf1e4a
Laurent Pouilloux <lolo@pouilloux.org>
2014-03-25 16:04:26 +0100

    Large improvements to plugin hdf5 that now returns values

--- kwapi/plugins/hdf5/v1.py
+++ kwapi/plugins/hdf5/v1.py
@@ -16,6 +16,7 @@
 
 """This blueprint defines all URLs and answers."""
 
+import time
 import flask
 import socket
 from execo_g5k import get_resource_attributes
@@ -51,40 +52,35 @@ def list_probes_ids():
     return response
 
 
-@blueprint.route('/measurements/')
+@blueprint.route('/metrics/')
 def retrieve_measurements():
     """Returns measurements."""
     hostname = socket.getfqdn().split('.')
     site = hostname[1] if len(hostname) >= 2 else hostname[0]
     
     args = flask.request.args
- 
+    probes = None
     if 'job_id' in args:
         job_info = get_resource_attributes('sites/' + site + '/jobs/' + args['job_id'])
         start_time = job_info['started_at']
         end_time = start_time + job_info['walltime']
         nodes = list(set(job_info['resources_by_type']['cores'])) 
         probes = [site + '.' + node.split('.')[0] for node in nodes]
-    else:
+    elif 'probes' in args:
+        LOG.info(args['probes'].split(','))
         probes = [site + '.' + node for node in args['probes'].split(',')]
-        start_time = args['start_time']
-        end_time = args['end_time']
-    
-    message = {}
-    for probe in probes:
-        df = read_hdf(cfg.CONF.hdf5_dir + '/store.h5', get_probe_path(probe), 
-            where=['index>=' +str(start_time), 'index<=' +str(end_time)])
-        measurements = df.to_json(orient='index')
-        LOG.info( probe)
-        LOG.info( measurements)
-        
-    message = {}
-    for probe in probes:
-        message[probe] = {
-            'start_time': start_time,
-            'end_time': end_time,
-            'kwh': kwh
-        }
+        start_time = args['start_time'] if 'start_time' in args else time.time() - 24 * 3600
+        end_time = args['end_time'] if 'end_time' in args else time.time()
+
+    if probes:
+        message = {'start_time': start_time, 'end_time': end_time, 'probes': {}}
+        for probe in probes:
+            message['probes'][probe] = []
+            df = read_hdf(cfg.CONF.hdf5_dir + '/store.h5', get_probe_path(probe), 
+                where=['index>=' +str(start_time), 'index<=' +str(end_time)])
+            for ts, mes in df.iterrows():
+                message['probes'][probe].append((ts, mes[0]))
+            
     response = flask.jsonify(message)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
