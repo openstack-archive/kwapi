9c37e384fcf6d2bdafa686e590d32ae9df1db9f5
clement-parisot <clement.parisot@telecomnancy.net>
2014-09-16 16:46:05 +0200

    simple rrd - TODO: Add thread functions instead of while True...

--- setup.py
+++ setup.py
@@ -43,8 +43,7 @@ setuptools.setup(
     ],
 
     packages=setuptools.find_packages(),
-    package_data={'kwapi.plugins.rrd': ['templates/*', 'static/*.css', 'static/*.png', 'static/*.jpg', 'static/*.js', 'static/select2/*'],
-                  'kwapi.plugins.live': ['templates/*', 'static/*.css', 'static/*.png', 'static/*.jpg', 'static/*.js', 'static/select2/*']},
+    package_data={'kwapi.plugins.live': ['templates/*', 'static/*.css', 'static/*.png', 'static/*.jpg', 'static/*.js', 'static/select2/*']},
 
     scripts=['bin/kwapi-g5k-conf'],
 
