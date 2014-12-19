19f94972a7735a9631780920a65e0313a7048325
Clement Parisot <clement.parisot@inria.fr>
2014-12-19 11:45:06 +0100

    [HDF5] Fix memory leak using PyTables (fix #8), Split file by variable period (month, week, day) and iterate over files when reading (fix #9)

--- kwapi/plugins/hdf5/hdf5_collector.py
+++ kwapi/plugins/hdf5/hdf5_collector.py
@@ -7,15 +7,9 @@ from threading import Lock, Timer
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from Queue import Queue
-import gc
-import psutil
 from tables import *
+from pandas import HDFStore
 
-def print_memory(m=None):
-    p = psutil.Process(os.getpid())
-    (rss, vms) = p.get_memory_info()
-    mp = p.get_memory_percent()
-    print("%-10.10s cur_mem->%.2f (MB),per_mem->%.2f" % (m, rss / 1000000.0, mp))
 
 LOG = log.getLogger(__name__)
 
@@ -60,9 +54,9 @@ hostname = socket.getfqdn().split('.')
 site = hostname[1] if len(hostname) >= 2 else hostname[0]
 # One queue per metric
 buffered_values = {
-    "power": Queue(600),
-    "network_in" : Queue(600),
-    "network_out": Queue(600),
+    "power": Queue(),
+    "network_in" : Queue(),
+    "network_out": Queue(),
 }
 
 def update_hdf5(probe, data_type, timestamp, metrics, params):
@@ -102,7 +96,8 @@ class HDF5_Collector:
                 raise
 
         """Retrieve next update date"""
-        self.save_date = datetime.strptime(cfg.CONF.start_date, "%Y/%m/%d")
+        self.start_date = datetime.strptime(cfg.CONF.start_date, "%Y/%m/%d")
+        self.save_date = self.start_date
         self.delta = relativedelta(days = cfg.CONF.split_days, weeks = cfg.CONF.split_weeks,
                             months = cfg.CONF.split_months)
         self.next_save_date = self.save_date + self.delta
@@ -113,13 +108,14 @@ class HDF5_Collector:
         LOG.info("Current save date: %s" % self.save_date)
         LOG.info("Next save date:    %s" % self.next_save_date)
         self.database = self.get_hdf5_file()
-        store = openFile(self.database, mode="w", title = "Fine grained measures")
+        store = openFile(self.database, mode="a", title = "Fine grained measures")
         store.close()
 
     def get_hdf5_file(self):
         if self.next_save_date <= datetime.now():
             self.save_date = self.next_save_date
             self.next_save_date += self.delta
+            LOG.info("Rotate to new file %s" % self.save_date.strftime("%Y_%m_%d"))
         return cfg.CONF.hdf5_dir + \
             '/%s_%s_%s' % (self.save_date.strftime("%Y_%m_%d"), self.data_type, 'store.h5')
 
@@ -140,7 +136,6 @@ class HDF5_Collector:
                                 np.array(zipped[0]),  # Timestp
                                 np.array(zipped[1]))  # measures
                 del self.measurements[probe][:]
-                #print "size %d" % buffered_values[self.data_type].qsize()
             buffered_values[self.data_type].task_done()
         # Flush datas
         LOG.info("FLUSH DATAS... %s", self.data_type)
@@ -156,37 +151,35 @@ class HDF5_Collector:
 
     def write_hdf5(self, probe, timestamps, measures):
         self.lock.acquire()
-        f = open_file(self.database, mode = "a")
+        f = openFile(self.get_hdf5_file(), mode = "a")
         try:
             path = get_probe_path(probe)
             if not path in f:
                 _, cluster, probe = path.split('/')
-                if not group in f.root:
-                    group = f.create_group("/", cluster, "cluster")
+                if not cluster in f.root:
+                    group = f.createGroup("/", cluster, "cluster")
+                group = f.getNode("/"+cluster)
                 if not path in f:
-                    table = f.create_table(group, probe, ProbeMeasures, "probe")
-            table = f.get_node(path)
+                    table = f.createTable(group, probe, ProbeMeasures, "probe")
+            table = f.getNode(path)
             for x in range(len(timestamps)):
                 table.row['timestamp'] = timestamps[x]
                 table.row['measure'] = measures[x]
                 table.row.append()
             table.flush()
-        except:
+        except Exception as e:
             LOG.error("Fail to add %s datas" % probe)
+            LOG.error("%s" % e)
         finally:
             f.flush()
             f.close()
             self.lock.release()
-            #print_memory("write2 %s" % probe)
-            #print gc.collect(),
-            #print gc.collect()
-            #print_memory("gc2 %s" % probe)
 
     def get_probes_list(self):
         probes = []
         self.lock.acquire()
         try:
-            store = HDFStore(self.database)
+            store = HDFStore(self.get_hdf5_file())
             for k in store.keys():
                 try:
                     # /site/host
@@ -203,57 +196,67 @@ class HDF5_Collector:
             self.lock.release()
         return probes
 
-    def rotate(self):
-        """
-        Rotate the log file to write in the correct location
-        This method is executed automatically after the timeout interval.
-        """
-
-        LOG.info('Rotate file')
-        # Stop writings
-        self.lock.acquire()
-        # Rotate file name
-        old_database = str(self.database)
-        old_save = self.next_save_date
-        self.database = self.get_hdf5_file()
-        #TODO Finish implementation
-        #if not self.database == old_database:
-        #    # Retrieving last written values
-        #    old_store = HDFStore(old_database)
-        #    for k in old_store.keys():
-        #        ts = read_hdf(old_database,
-        #                      k,
-        #                      where="index>'%s'"%old_save)
-        #        # Append missing values to new file
-        #        with get_store(self.database) as new_store:
-        #            new_store.append(k, ts)
-        #            new_store.flush(fsync=True)
-
-        # Schedule periodic execution of this function
-        timer = Timer(3600.0, self.rotate)
-        timer.daemon = True
-        timer.start()
-        # Release lock
-        self.lock.release()
+    def get_hdf5_files(self, start_time, end_time):
+        list_files = []
+        if end_time < start_time:
+            return list_files
+        else:
+            try:
+                file_date = datetime.fromtimestamp(float(start_time))
+                end_date  = datetime.fromtimestamp(float(end_time))
+                if file_date < self.start_date:
+                    file_date = self.start_date
+                if end_date > datetime.now():
+                    end_date = datetime.now()
+                start_date = self.start_date
+                while start_date < file_date:
+                    start_date += self.delta
+                file_date = start_date - self.delta
+                while file_date < end_date:
+                    s = cfg.CONF.hdf5_dir + '/%s_%s_%s' % (file_date.strftime("%Y_%m_%d"), self.data_type, 'store.h5')
+                    if os.path.isfile(s):
+                        list_files.append(s)
+                    file_date += self.delta
+                return list_files
+            except Exception as e:
+                LOG.error("Get file list: %s" % e)
+                return []
 
     def select_probes_datas(self, probes, start_time, end_time):
         message = dict()
         self.lock.acquire()
-        for probe in probes:
-            path = get_probe_path(probe)
-            if path:
-                message[probe] = dict()
-                message[probe]["uid"] = probe.split('.')[1]
-                message[probe]["to"] =  int(end_time)
-                message[probe]["from"] = int(start_time)
-                try:
-                    ts = read_hdf(self.database,
-                                  path,
-                                  where=["index>='%s'"%to_datetime(start_time, unit='s'),
-                                         "index<='%s'"%to_datetime(end_time, unit='s')])
-                    message[probe]['values'] = list(ts.values)
-                    message[probe]['timestamps'] = list(ts.index.astype(np.int64) // 10**9)
-                except:
-                    message[probe]['values'] = ['Unknown probe']
-        self.lock.release()
+        list_files = self.get_hdf5_files(start_time, end_time)
+        try:
+            for filename in list_files:
+                for probe in probes:
+                    if not probe in message:
+                        # Init message for probe
+                        message[probe] = dict()
+                        message[probe]["uid"] = probe.split('.')[1]
+                        message[probe]["to"] =  int(end_time)
+                        message[probe]["from"] = int(start_time)
+                        message[probe]['values'] = []
+                        message[probe]['timestamps'] = []
+                    path = get_probe_path(probe)
+                    if path:
+                        f = openFile(filename, mode = "r")
+                        try:
+                            ts = []
+                            table = f.getNode(path)
+                            ts = [(x["timestamp"], x["measure"]) \
+                                  for x in table.where("(timestamp >= %s) & (timestamp <= %s)" \
+                                                       % (start_time, end_time))]
+                            timestamps, values = zip(*ts)
+                            message[probe]['values'] += list(values)
+                            message[probe]['timestamps'] += list(timestamps)
+                        except Exception as e:
+                            message[probe]['values'] = ['Unknown probe']
+                            LOG.error("%s" % e)
+                        finally:
+                            f.close()
+        except Exception as e:
+            message[probe]['values'] = ['Unknown probe']
+            LOG.error("%s" % e)
+        finally:
+            self.lock.release()
         return message
