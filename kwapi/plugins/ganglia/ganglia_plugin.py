1d989137581247b2c5b52662e1e9f55f44058cc4
Clement Parisot <clement.parisot@inria.fr>
2015-05-28 15:04:58 +0200

    [GANGLIA] Compatibility with new config file

--- kwapi/plugins/ganglia/ganglia_plugin.py
+++ kwapi/plugins/ganglia/ganglia_plugin.py
@@ -47,26 +47,35 @@ site = hostname[1] if len(hostname) >= 2 else hostname[0]
 ganglia = GMetric(cfg.CONF.ganglia_server[0])
 ip_probe = {}
 
-def update_rrd(probe, probe_names, data_type, timestamp, metrics, params):
+def update_rrd(probe_uid, probes_names, data_type, timestamp, metrics, params):
     """Retrieve hostname and address"""
     if not data_type == 'power':
         return
-    probe_site = probe.split('.')[0]
-    probe_id = str(".".join(probe.split('.')[1:])) 
-    if not probe_id in ip_probe:
-        # Hack to know if it is multi-probe
-        if probe_id.count('-') > 1:
-            # Ignore multi-probe
-            return
-        hostname = "%s.%s.grid5000.fr" % (probe_id, probe_site)
-        ip = socket.gethostbyname(hostname)
-        ip_probe[probe_id] = (ip, hostname)
-    ganglia.send(
-        name='pdu2',
-        units='Watts',
-        type='uint16',
-        value=int(metrics),
-        hostname='%s:%s' % (ip_probe[probe_id][0], ip_probe[probe_id][1]),
-        spoof=True
-    )
+    if not type(probes_names) == list:
+        probes_names= [probes_names]
+    if len(probes_names) > 1:
+        # Multiprobes are not exported
+        return
+    for probe in probes_names:
+        probe_site = probe.split('.')[0]
+        probe_id = str(".".join(probe.split('.')[1:])) 
+        if not probe_id in ip_probe:
+            hostname = "%s.%s.grid5000.fr" % (probe_id, probe_site)
+            try:
+                ip = socket.gethostbyname(hostname)
+                ip_probe[probe_id] = (ip, hostname)
+            except:
+                LOG.error("Fail to retrieve %s ip", hostname)
+                ip_probe[probe_id] = None
+                continue
+        if not ip_probe[probe_id]:
+            continue
+        ganglia.send(
+            name='pdu2',
+            units='Watts',
+            type='uint16',
+            value=int(metrics),
+            hostname='%s:%s' % (ip_probe[probe_id][0], ip_probe[probe_id][1]),
+            spoof=True
+        )
     return
